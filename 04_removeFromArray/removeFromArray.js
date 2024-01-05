b7849aa8071c731de7e753cdcbe3764d964ff073
Soleguard <139895712+Soleguard@users.noreply.github.com>
2024-01-05 17:33:54 -0500

    Add filter function
    This version includes a variable with an anonymous function that filters
    and removes the given element and returns the new array without that
    element.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, element) {
+    
+    // Declare variable (anonymous function) for new array
+    const new_Array = array.filter(function(given) {
+        return given !== element;
+    });
 
+    // Return new array
+    return new_Array;
 };
 
 // Do not edit below this line
