9ffb71be45f25e29d31296c203fbe2250cdf228c
Soleguard <139895712+Soleguard@users.noreply.github.com>
2024-01-05 10:09:15 -0500

    Add parameter and separate variables for reversing string
    This version includes a parameter for the given string and a variable
    for splitting, reversing, and joining the string. A return
    statement is included for the final result.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,16 @@
-const reverseString = function() {
+const reverseString = function(word) {
+
+    // Split word into array of chars
+    let split_Word = word.split('');
+
+    // Reverse array
+    let reversed_Array = split_Word.reverse();
+
+    // Join array into string
+    let joined_String = reversed_Array.join('');
+
+    // Return final result
+    return joined_String;
 
 };
 
