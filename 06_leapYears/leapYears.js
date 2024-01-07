8f541e2979b889e0c6a769549138169ffdf8443c
Soleguard <139895712+Soleguard@users.noreply.github.com>
2024-01-07 13:36:51 -0500

    Add verifier for leap year
    This version uses an if statement to verify the leap year via
    divisibility by 100 and 400 and 4, returning true if so and false if
    not.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,21 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    // Determine if year is divisible by 100 and not divisible by 400
+    if (year % 100 == 0 && !(year % 400 == 0))
+    {
+        return false;
+    }
+
+    // Determine if value is divisible by 4
+    else if (year % 4 == 0)
+    {
+        return true;
+    }
+
+    else
+    {
+        return false;
+    }
 
 };
 
