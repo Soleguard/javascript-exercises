d3ce118c3ce1136d4f848f2d90418a5ddda32f90
Soleguard <139895712+Soleguard@users.noreply.github.com>
2024-01-07 11:05:53 -0500

    Include variables, verifiers, loop, and returner
    This version includes sum and larger and smaller number variables as
    well as verifiers for non-number and negative values. In addition, it
    assigns the larger and smaller value, loops through it, and returns the
    sum.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,42 @@
-const sumAll = function() {
+const sumAll = function(start, stop) {
+
+    let sum;
+    let larger_Number;
+    let smaller_Number;
+
+    // Check for non-number parameters
+    if (!(typeof start === "number") || !(typeof stop === "number"))
+    {
+        return 'ERROR';
+    }
+
+    // Check for negative numbers
+    else if (start < 0 || stop < 0)
+    {
+        return 'ERROR';
+    }
+
+    // Assign larger and smaller number
+    if (stop > start)
+    {
+        larger_Number = stop;
+        smaller_Number = start;
+    }
+ 
+    else
+    {
+        larger_Number = start;
+        smaller_Number = stop;
+    }
+
+    // Loop from larger to smaller number
+    for (let i = larger_Number; i >= smaller_Number; i--) {
+        // Sum i
+        sum += i;
+    }
+
+    // Return sum
+    return sum;
 
 };
 
