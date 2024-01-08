fa9a8e961f207936c937bc3efc0a75b86e3e0fc9
Soleguard <139895712+Soleguard@users.noreply.github.com>
2024-01-07 21:26:15 -0500

    Add parameter, variable, and converters
    This version uses converters for Celsius and Fahrenheit, including
    temperature parameter, result variable, conversion calculation, rounder
    to single decimal, and number converter.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,39 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+
+  // Declare and initialize result
+  let result = 0;
+
+  // Calculate conversion
+  result = ((temperature) - 32) * (5 / 9);
+
+  // Round to one decimal
+  result.toFixed(1);
+
+  // Convert to number
+  Number(result)
+
+  // Return result
+  return result;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+
+  // Declare and initialize result
+  let result = 0;
+
+  // Calculate conversion
+  result = ((temperature) * (9 / 5)) + 32;
+
+  // Round to one decimal
+  result.toFixed(1);
+
+  // Convert to number
+  Number(result)
+
+  // Return result
+  return result;
+
 };
 
 // Do not edit below this line
